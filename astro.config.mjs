fb4b4956ff8dcecc2619c6fcdbdcf176bd1082c5
wilmerrojasbo <wilmerrojasbo@gmail.com>
2024-08-22 17:25:16 -0600

    Add Vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://wilmerrojasbo.github.io',
+  //site: 'https://wilmerrojasbo.github.io',
   integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: 'en',
@@ -13,5 +14,7 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+  output: 'server',
+  adapter: vercel()
 })
